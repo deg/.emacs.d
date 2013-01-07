4142f3512ef9d85e3f46505440e8cf4c13a4cf5c
David Goldfarb <deg@degel.com>
2013-01-08 01:35:00 +0200

    Auto-compile emacs-lisp files.

--- init.el
+++ init.el
@@ -12,6 +12,14 @@
 ;;;...
 
 
+(defun byte-compile-current-buffer ()
+  "`byte-compile' current buffer if it's emacs-lisp-mode and compiled file exists."
+  (interactive)
+  (when (and (eq major-mode 'emacs-lisp-mode)
+             (file-exists-p (byte-compile-dest-file buffer-file-name)))
+    (byte-compile-file buffer-file-name)))
+
+(add-hook 'after-save-hook 'byte-compile-current-buffer)
 
 
 ;;; Emacs behavior - I suppose my basic goal is to look like Epsilon and my memories of ZMacs.
