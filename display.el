4d39f4ed35a36a842b86e34af425d7f9ce70ab43
David Goldfarb <deg@degel.com>
2013-03-21 15:59:10 +0200

    Don't try to use x11 on windows

--- display.el
+++ display.el
@@ -61,12 +61,12 @@ Warning: tested on Windows Vista only."
 (setq initial-scratch-message nil)
 (menu-bar-mode -1)
 (add-hook 'window-setup-hook (lambda () (tool-bar-mode -1)))
-(defun x11-maximize-frame ()
-  "Maximize the current frame (to full screen)"
-  (interactive)
-  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
-  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0)))
-(x11-maximize-frame)
+(unless (string-equal system-type "windows-nt")
+  (defun x11-maximize-frame ()
+    "Maximize the current frame (to full screen)"
+    (interactive)
+    (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+    (x-send-client-message nil 0 nil "_NET_WM_STATE" 32 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0)))  (x11-maximize-frame))
 
 
 ;; Remove cognitive clutter
