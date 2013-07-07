7edbc7ec540532a53e5a517f4812d3832cc1736f
David Goldfarb <deg@degel.com>
2013-07-07 22:03:04 +0300

    Add emacs bindings for goto-line and ansi-term

--- bindings.el
+++ bindings.el
@@ -10,11 +10,13 @@
 (global-set-key (kbd "C-h a") 'apropos)
 (global-set-key (kbd "C-h C-a") 'apropos-command)
 (global-set-key (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "C-x g") 'goto-line)
 
 ;; New bindings
 (global-set-key (kbd "C-c TAB") 'browse-url)
 (global-set-key (kbd "C-x !") 'shell)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "C-x !") 'ansi-term)
 
 ;; My functions
 (global-set-key (kbd "C-x C-r") 'ido-recentf-open)
@@ -25,5 +27,3 @@
 (global-set-key (kbd "C-x u") 'undo-tree-visualize)
 (global-set-key [f9] 'undo-tree-undo)
 (global-set-key [f10] 'undo-tree-redo)
-
-
