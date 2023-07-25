0eabfb76b0306888f05222bb0b7946f20b293214
David Goldfarb <deg@degel.com>
2023-07-25 19:05:53 +0300

    Improve python venv handling

--- python.el
+++ python.el
@@ -24,10 +24,30 @@
 (global-set-key (kbd "C-c #") 'elpy-shell-clear-shell)
 
 
+;; Automatically activate project venv, if it exists
+(defun my-pyvenv-activate-dir ()
+  "Search for venv directory and activate virtualenv there if found."
+  (let ((venv-dir (locate-dominating-file default-directory ".venv")))
+    (when venv-dir
+      (pyvenv-activate (concat venv-dir ".venv")))))
+
+(add-hook 'python-mode-hook 'my-pyvenv-activate-dir)
+
+
 ;; Fix problem with Elpy not finding local imports.
+(defun my-bounded-locate-dominating-file (dir bound file-name)
+  "Find file-name in dir or its parent directories, but do not go above bound."
+  (let* ((expanded-dir (expand-file-name dir))
+         (expanded-bound (expand-file-name bound))
+         (file-path (expand-file-name file-name expanded-dir)))
+    (cond ((equal expanded-dir expanded-bound) expanded-bound)
+          ((file-exists-p file-path) expanded-dir)
+          (t (my-bounded-locate-dominating-file (file-name-directory (directory-file-name expanded-dir)) expanded-bound file-name)))))
+
 (defun my-set-pythonpath ()
-  (let ((project-root (locate-dominating-file default-directory ".projectile")))
+  (let ((project-root (my-bounded-locate-dominating-file default-directory (locate-dominating-file default-directory ".git") ".projectile")))
     (when project-root
       (setenv "PYTHONPATH" (expand-file-name project-root)))))
+
 (add-hook 'pyvenv-post-activate-hooks 'my-set-pythonpath)
 (add-hook 'pyvenv-post-deactivate-hooks 'my-set-pythonpath)
